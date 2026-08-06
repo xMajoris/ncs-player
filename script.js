2b3c91452d7639a951027a4066ed95672a020216
Samuel Hagas <samuelhagas918@gmail.com>
2026-08-06 18:37:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const playlist = [
-  { id: "A_AtqKMqPUE", title: "Defeat The Night", artist: "JPB" },
-  { id: "6FNHe3kf8_s", title: "Different Heaven", artist: "Nekozilla" },
-  { id: "bJ-ldzTkJEA", title: "Candyland", artist: "Tobu" },
-  { id: "J2X5mJ3HDYE", title: "Invincible", artist: "DEAF KEV" },
-  { id: "3nQNiWdeH2Q", title: "Heroes Tonight", artist: "Janji" },
-  { id: "1KqQQHpQc8w", title: "Cloud 9", artist: "Itro & Tobu" },
-  { id: "p7ZsBPK656s", title: "On & On", artist: "Cartoon" },
-  { id: "C6IaUMAg3Dc", title: "We Are", artist: "Jo Cohen & Sex Whales" },
-  { id: "zyXmsVwZqX4", title: "Why We Lose", artist: "Cartoon" },
-  { id: "L7kF4MXXCoA", title: "Dreams pt. II", artist: "Lost Sky" },
-  { id: "n4tK7LYFxI0", title: "Shine", artist: "Spektrem" },
-  { id: "yHLtE1wFeRQ", title: "Linked", artist: "Jim Yosef & Anne Yvette" },
-  { id: "q1ULJ92aldE", title: "Feel Good", artist: "Syn Cole" },
+  { file: "songs/Defeat the Night.mp3", cover: "assets/NCS.png", title: "Defeat The Night", artist: "JPB" },
+  { file: "songs/Blank.mp3", cover: "assets/NCS.png", title: "Blank", artist: "Disfigure" },
+  { file: "songs/Symbolism.mp3", cover: "assets/NCS.png", title: "Symbolism", artist: "Electro-Light" },
+  { file: "songs/Sky High.mp3", cover: "assets/NCS.png", title: "Sky High", artist: "Elektronomia" },
+  { file: "songs/Nekozilla.mp3", cover: "assets/NCS.png", title: "Nekozilla", artist: "Different Heaven" },
+  { file: "songs/Invincible.mp3", cover: "assets/NCS.png", title: "Invincible", artist: "DEAF KEV" },
+  { file: "songs/Heroes Tonight.mp3", cover: "assets/NCS.png", title: "Heroes Tonight", artist: "Janji" },
+  { file: "songs/On & On.mp3", cover: "assets/NCS.png", title: "On & On", artist: "Cartoon" },
+  { file: "songs/Superhero.mp3", cover: "assets/NCS.png", title: "Superhero", artist: "Alex Hagen" },
+  { file: "songs/Hellcat.mp3", cover: "assets/NCS.png", title: "Hellcat", artist: "Desmeon" },
+  { file: "songs/C U Again.mp3", cover: "assets/NCS.png", title: "C U Again", artist: "Futuristik ft. Mikk mae" },
+  { file: "songs/Eclipse.mp3", cover: "assets/NCS.png", title: "Eclipse", artist: "Jim Yosef" },
+  { file: "songs/Light It Up.mp3", cover: "assets/NCS.png", title: "Light It Up", artist: "Robin Hustin & Tobimorrow" },
+  { file: "songs/Ark.mp3", cover: "assets/NCS.png", title: "Ark", artist: "Ship Wrek" },
+  { file: "songs/Turn It Up.mp3", cover: "assets/NCS.png", title: "Turn It Up", artist: "Tobu" },
+  { file: "songs/Why Do I.mp3", cover: "assets/NCS.png", title: "Why Do I?", artist: "Unknown Brain ft. Bri Tolani" },
+  { file: "songs/MATAFAKA.mp3", cover: "assets/NCS.png", title: "MATAFAKA", artist: "Unknown Brain ft. Marvin Divine" },
+  { file: "songs/Stronger.mp3", cover: "assets/NCS.png", title: "Stronger (Raiko Remix)", artist: "Prismo" },
+  { file: "songs/Fearless.mp3", cover: "assets/NCS.png", title: "Fearless", artist: "TULE" },
+  { file: "songs/Mortals.mp3", cover: "assets/NCS.png", title: "Mortals", artist: "Warriyo ft. Laura Brehm" },
+  { file: "songs/Mortals Funk Remix.mp3", cover: "assets/NCS.png", title: "Mortals Funk Remix", artist: "Warriyo & LXNGVX" },
+  { file: "songs/We Are.mp3", cover: "assets/NCS.png", title: "We Are", artist: "Jo Cohen & Sex Whales" },
+  { file: "songs/Why we lose.mp3", cover: "assets/NCS.png", title: "Why We Lose", artist: "Cartoon" },
+  { file: "songs/Dreams pt. II.mp3", cover: "assets/NCS.png", title: "Dreams pt. II", artist: "Lost Sky" },
+  { file: "songs/Shine.mp3", cover: "assets/NCS.png", title: "Shine", artist: "Spektrem" },
+  { file: "songs/Linked.mp3", cover: "assets/NCS.png", title: "Linked", artist: "Jim Yosef & Anne Yvette" },
+  { file: "songs/Feel Good.mp3", cover: "assets/NCS.png", title: "Feel Good", artist: "Syn Cole" }
 ];
 
 function embaralharPlaylist(lista) {
@@ -24,9 +38,7 @@ embaralharPlaylist(playlist);
 
 let currentSongIndex = 0;
 let isPlaying = false;
-let player;
-let isMuted = false;
-let previousVolume = 80;
+let previousVolume = 0.8;
 
 const playBtn = document.querySelector(".play");
 const nextBtn = document.querySelector(".next");
@@ -34,128 +46,130 @@ const prevBtn = document.querySelector(".prev");
 const albumArt = document.querySelector("#albumCover");
 const songTitleDisplay = document.querySelector(".music-title");
 
-const volumeWrapper = document.querySelector(".volume-control-wrapper");
 const volIconBtn = document.querySelector(".vol-icon");
 const volumeSlider = document.querySelector(".volume-slider");
 
-const tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-const firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+const audioElement = new Audio();
+audioElement.volume = previousVolume;
+volumeSlider.value = previousVolume * 100;
 
-window.onYouTubeIframeAPIReady = function() {
-  player = new YT.Player('ytPlayer', {
-    height: '0',
-    width: '0',
-    videoId: playlist[currentSongIndex].id,
-    playerVars: { 'playsinline': 1, 'controls': 0 },
-    events: { 'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange }
-  });
-};
+function digitarSaudacao() {
+  const agora = new Date();
+  const hora = agora.getHours();
+  let textoSaudacao = "Welcome";
 
-function onPlayerReady(event) {
-  updateMetadata();
-  player.setVolume(previousVolume);
-  volumeSlider.value = previousVolume;
+  if (hora >= 5 && hora < 12) {
+    textoSaudacao = "Good Morning";
+  } else if (hora >= 12 && hora < 18) {
+    textoSaudacao = "Good Afternoon";
+  } else {
+    textoSaudacao = "Good Evening";
+  }
+
+  const welcomeElement = document.querySelector(".welcome");
+  if (!welcomeElement) return;
+
+  welcomeElement.innerText = "";
+  let i = 0;
+  const velocidade = 100;
+
+  function escrever() {
+    if (i < textoSaudacao.length) {
+      welcomeElement.innerText += textoSaudacao.charAt(i);
+      i++;
+      setTimeout(escrever, velocidade);
+    }
+  }
+
+  escrever();
 }
 
-function updateMetadata() {
+digitarSaudacao();
+
+function loadSong(shouldPlay = false) {
   const currentSong = playlist[currentSongIndex];
+  
   albumArt.style.opacity = 0;
   songTitleDisplay.style.opacity = 0;
+
   setTimeout(() => {
-    albumArt.src = `https://img.youtube.com/vi/${currentSong.id}/hqdefault.jpg`;
+    albumArt.src = currentSong.cover;
     songTitleDisplay.innerText = `${currentSong.title} - ${currentSong.artist}`;
     albumArt.style.opacity = 1;
     songTitleDisplay.style.opacity = 1;
   }, 200);
-}
 
-function onPlayerStateChange(event) {
-  if (event.data === YT.PlayerState.PLAYING) {
-    isPlaying = true;
-    playBtn.innerText = "⏸";
-  } else if (event.data === YT.PlayerState.PAUSED) {
-    isPlaying = false;
-    playBtn.innerText = "▶";
+  audioElement.src = currentSong.file;
+  audioElement.load();
+
+  if (shouldPlay) {
+    audioElement.play().then(() => {
+      isPlaying = true;
+      playBtn.innerText = "⏸";
+    }).catch(e => console.log(e));
   }
 }
 
-function loadSong() {
-  updateMetadata();
-  if (player && typeof player.loadVideoById === 'function') {
-    player.loadVideoById(playlist[currentSongIndex].id);
-    isPlaying = true;
-    playBtn.innerText = "⏸";
-  }
-}
+loadSong(false);
 
 playBtn.addEventListener("click", function () {
-  if (!player) return;
-  if (isPlaying) player.pauseVideo();
-  else player.playVideo();
+  if (isPlaying) {
+    audioElement.pause();
+    isPlaying = false;
+    playBtn.innerText = "▶";
+  } else {
+    audioElement.play().then(() => {
+      isPlaying = true;
+      playBtn.innerText = "⏸";
+    }).catch(e => console.log(e));
+  }
 });
 
 nextBtn.addEventListener("click", function () {
   currentSongIndex++;
   if (currentSongIndex >= playlist.length) currentSongIndex = 0;
-  loadSong();
+  loadSong(isPlaying);
 });
 
 prevBtn.addEventListener("click", function () {
   currentSongIndex--;
   if (currentSongIndex < 0) currentSongIndex = playlist.length - 1;
-  loadSong();
+  loadSong(isPlaying);
+});
+
+audioElement.addEventListener("ended", function() {
+  currentSongIndex++;
+  if (currentSongIndex >= playlist.length) currentSongIndex = 0;
+  loadSong(true);
 });
 
 volIconBtn.addEventListener("click", function() {
-  if (!volumeWrapper.classList.contains('show-slider')) {
-    volumeWrapper.classList.add('show-slider');
-  } else {
-    toggleMute();
-  }
+  toggleMute();
 });
 
 function toggleMute() {
-  if (!player) return;
-  if (player.isMuted()) {
-    player.unMute();
-    isMuted = false;
+  if (audioElement.muted) {
+    audioElement.muted = false;
     volIconBtn.innerText = "🔊";
-    volumeSlider.value = previousVolume; 
+    volumeSlider.value = previousVolume * 100;
   } else {
-    player.mute();
-    isMuted = true;
-    previousVolume = player.getVolume();
+    audioElement.muted = true;
+    previousVolume = audioElement.volume;
     volIconBtn.innerText = "🔇";
     volumeSlider.value = 0;
   }
 }
 
-document.addEventListener('click', function(event) {
-  const isClickInside = volumeWrapper.contains(event.target);
-  if (!isClickInside && volumeWrapper.classList.contains('show-slider')) {
-    volumeWrapper.classList.remove('show-slider');
-  }
-});
-
 volumeSlider.addEventListener("input", function() {
-  if (!player) return;
-  
   const newVol = this.value;
-  player.setVolume(newVol);
+  audioElement.volume = newVol / 100;
+  audioElement.muted = false;
   
   if (newVol == 0) {
     volIconBtn.innerText = "🔇";
-    player.mute();
-    isMuted = true;
+  } else if (newVol > 50) {
+    volIconBtn.innerText = "🔊";
   } else {
-    player.unMute();
-    isMuted = false;
-    if (newVol > 50) {
-        volIconBtn.innerText = "🔊";
-    } else {
-        volIconBtn.innerText = "🔉";
-    }
+    volIconBtn.innerText = "🔉";
   }
 });
