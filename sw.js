c4decb28d05bc0ec226b8801df7d4d44136a44ff
Samuel Hagas <samuelhagas918@gmail.com>
2026-08-13 19:52:37 -0300

    again v2

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
 const CACHE_NAME = "ncsmini-v1";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/assets/NCS.png",
-  "/assets/NCS(white).jpg",
-  "/assets/icon-192.png",
-  "/assets/icon-512.png",
+  "/ncs-player/",
+  "/ncs-player/index.html",
+  "/ncs-player/style.css",
+  "/ncs-player/script.js",
+  "/ncs-player/manifest.json",
+  "/ncs-player/assets/NCS.png",
+  "/ncs-player/assets/NCS(white).jpg",
+  "/ncs-player/assets/icon-192.png",
+  "/ncs-player/assets/icon-512.png",
 ];
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Cache aberto");
+      console.log("Cache aberto!");
       return cache.addAll(urlsToCache);
     }),
   );
